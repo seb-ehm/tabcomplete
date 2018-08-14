f540486a6cf53a3f2f1a2ffcbcf1287189f50c84
Sebastian Ehmann <sebastian.ehmann@web.de>
2018-08-14 15:40:45 +0200

    Fixed output

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ int main(int argc, _TCHAR* argv[]) {
 	//std::cin >> a;
 	Consolero console;
 	auto line = console.Cin();
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> cv;
-    std::string convertedString = cv.to_bytes(line);
-    std::cout << "\n" << convertedString  << "\n";
+    std::cout << "\n" << line << "\n";
+    //std::wstring_convert<std::codecvt_utf16<wchar_t, 0x10ffff, std::little_endian>> cv;
+    //std::string convertedString = cv.to_bytes(line);
+    //std::cout << "\n" << convertedString  << "\n";
 
 }
